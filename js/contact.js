632dc10e3813a6922f78759168a537f44c372d68
Clasycow <lepharris@icloud.com>
2022-12-31 21:14:04 -0500

    Website Data Change

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,36 @@
-document.querySelector('#contact-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    e.target.elements.name.value = '';
-    e.target.elements.email.value = '';
-    e.target.elements.message.value = '';
-  });
+$.validator.addMethod("mailcheck", function (a) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(a);
+});
+$("#form_val").validate({
+	rules: {
+		email: {
+			required: true,
+			mailcheck: true
+		},
+		name: {
+			required: true
+		},
+		subject: {
+			required: true
+		},
+		message: {
+			required: true
+		}
+	},
+	messages: {
+		email: {
+			required: "Please enter a email address",
+			email: "Please enter a valid email address"
+		},
+		name: {
+			required: "Please enter your name"
+		},
+		subject: {
+			required: "Please enter a subject",
+		},
+		message: {
+			required: "Please enter a message"
+		}
+	}
+
+});
